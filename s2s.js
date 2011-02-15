2958dac691d4a9fe4dcd3b2a14fbde5810da9524
khotyn <hting1@gmail.com>
2011-02-16 00:00:17 +0800

    Put back bg.html

--- s2s.js
+++ s2s.js
@@ -1,5 +1,15 @@
 var searchQuery;
 
+chrome.extension.sendRequest({'type': 'get_map_data'}, function(response) {
+		var u2sMappings = response.u2sMappings;
+		for (var i = 0; i < u2sMappings.length; i++) {
+		var reg = new RegExp(u2sMappings[i].url)
+		if(document.URL.match(reg)) {
+			chrome.extension.sendRequest({'type': 'get_tab_id'}); 
+		}
+		}
+	});
+
 function focusQuery(){
 	searchQuery.focus();
 }
@@ -8,18 +18,18 @@ function keyPress() {
 	var key = event.keyCode;
 
 	if(key == 191) {
-		chrome.extension.sendRequest({}, function(response){
+		chrome.extension.sendRequest({'type': 'get_map_data'}, function(response){
 				var u2sMappings = response.u2sMappings;
 				for(var i = 0; i < u2sMappings.length; i++) {
-					var reg = new RegExp(u2sMappings[i].url)
-					if (document.URL.match(reg)) {
-						searchQuery = document.getElementById(u2sMappings[i].search_id);
-							if(searchQuery != null && document.activeElement != searchQuery && document.activeElement.nodeName != 'TEXTAREA' && document.activeElement.nodeName != 'INPUT') {
-								setTimeout('focusQuery()', 10);
-							}
-					}
+				var reg = new RegExp(u2sMappings[i].url)
+				if (document.URL.match(reg)) {
+				searchQuery = document.getElementById(u2sMappings[i].search_id);
+				if(searchQuery != null && document.activeElement != searchQuery && document.activeElement.nodeName != 'TEXTAREA' && document.activeElement.nodeName != 'INPUT') {
+				setTimeout('focusQuery()', 10);
 				}
-			});
+				}
+				}
+				});
 	}
 }
 
