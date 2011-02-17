3361f3837983219ec098ce21e0dce2afc1a8ee86
khotyn <hting1@gmail.com>
2011-02-18 00:01:26 +0800

    Totally rewrite

--- options.js
+++ options.js
@@ -6,15 +6,13 @@ $(function(){
 		show_rules();
 		var bg_obj = chrome.extension.getBackgroundPage();
 		if( bg_obj.quick_add == true ) {
-			//toggle_show_add_rules();
-			if(bg_obj.url != undefined && bg_obj.search_id != undefined) {
+			if(bg_obj.url != undefined && bg_obj.tree_route != undefined) {
 				$('#url').attr('value', bg_obj.url);
-				$('#searchId').attr('value', bg_obj.search_id);
+				$('#tree_route').attr('value', bg_obj.tree_route);
 			}
 			bg_obj.quick_add = false;
 			bg_obj.url = undefined;
-			bg_obj.search_id = undefined;
-			//toggle_show_add_rules();
+			bg_obj.tree_route= undefined;
 			setTimeout('toggle_show_add_rules()', 10);
 		}
 		});
@@ -26,26 +24,28 @@ function remove_rules(){
 
 function add_rules(){
 	var url = $('#url').attr('value'); 
-	var searchId = $('#searchId').attr('value');
+	var type = $('#type').attr('value');
+	var tree_route = $('#tree_route').attr('value');
 
-	if( url == undefined || url.trim() == '' || searchId == undefined || searchId.trim() == '') {
+	if( url == undefined || url.trim() == '' || type == undefined || type.trim() == '' || tree_route == undefined || tree_route.trim() == '') {
 		return;
 	}
 
 	var u2sMappings = localStorage["u2sMappings"];
 
 	if(u2sMappings == undefined) {
-		var jsonObj = {"u2sMappings":[{"id":1, "url": url, "search_id": searchId}],"maxId":1};
+		var jsonObj = {"u2sMappings":[{"id":1, "url": url, "type": type, 'tree_route': tree_route}],"maxId":1};
 		localStorage["u2sMappings"] = JSON.stringify(jsonObj);
 	} else {
 		var u2sJson = JSON.parse(u2sMappings);
-		var jsonObj = [{"id": ++ u2sJson.maxId, "url": url, "search_id": searchId}];
+		var jsonObj = [{"id": ++ u2sJson.maxId, "url": url, "type": type, 'tree_route': tree_route}];
 		u2sJson.u2sMappings = u2sJson.u2sMappings.concat(jsonObj);
 		localStorage["u2sMappings"] = JSON.stringify(u2sJson);
 	}
 
 	$('#url').attr('value', '');
-	$('#searchId').attr('value', '');
+	$('#type').attr('value', '');
+	$('#tree_route').attr('value', '');
 	show_rules();
 	toggle_show_add_rules(); // to hide the add rule layer.
 }
@@ -55,10 +55,10 @@ function show_rules(){
 	$('#rules > table').children().remove();
 	$('#no_rules').remove();
 	if(u2sMappings != undefined) {
-		$('#rules > table').append('<tr><th width="50%">URL</th><th width="25%">Search input id</th><th width="25%">Edit</th><tr>');
+		$('#rules > table').append('<tr><th width="50%">URL</th><th width="25%">Type</th><th width="25%">Edit</th><tr>');
 		var jsonObj = JSON.parse(u2sMappings);
 		for(var count = 0; count < jsonObj.u2sMappings.length; count++) {
-			$('#rules > table').append('<tr><td>' + jsonObj.u2sMappings[count].url + '</td><td>'  + jsonObj.u2sMappings[count].search_id + '</td><td><button name="edit">Edit</button><button name="delete">Delete</button><input type="hidden" value="' + jsonObj.u2sMappings[count].id + '"/></td></tr>');
+			$('#rules > table').append('<tr><td>' + jsonObj.u2sMappings[count].url + '</td><td>'  + jsonObj.u2sMappings[count].type + '</td><td><button name="edit">Edit</button><button name="delete">Delete</button><input type="hidden" value="' + jsonObj.u2sMappings[count].id + '"/></td></tr>');
 		}
 		$('button[name=edit]').bind('click', edit_rule);
 		$('button[name=delete]').bind('click', remove_rule);
@@ -73,10 +73,6 @@ function toggle_show_add_rules() {
 	} else {
 		var windowWidth = document.body.clientWidth;
 		var windowHeight = document.body.clientHeight;
-		//while (windowWidth <= 0 || windowHeight <= 0 ) {
-		//	windowWidth = document.body.clientWidth;
-		//	windowHeight = document.body.clientHeight;
-		//}
 		var popwinWidth = $('#popwin').width();
 		var popwinHeight = $('#popwin').height();
 		$('#popwin').css('left', (windowWidth - popwinWidth) / 2).css('top', (windowHeight - popwinHeight) / 2).css('visibility', 'visible');
@@ -96,7 +92,13 @@ function edit_rule(event) {
 	}
 	var tr = $(event.target).parent().parent();
 	tr.children().remove();
-	tr.append('<td><input type="text" class="text" style="width:100%;" value="' + rule.url + '"/></td><td><input class="text" style="width:100%;" type="text" value="' + rule.search_id +'"/></td><td><button name="done">Done</button><button name="delete">Delete</button><input type="hidden" value="' + rule.id + '"/></td>');
+	var option_ele;
+	if(rule.type == 'regex') {
+		option_ele = '<option value="normal">Normal</option><option value="regex" selected>Regex</option>';
+	} else {
+		option_ele = '<option value="normal" selected>Normal</option><option value="regex">Regex</option>';
+	}
+	tr.append('<td><input type="text" class="text" style="width:100%;" value="' + rule.url + '"/></td><td><select>' + option_ele + '</select></td><td><button name="done">Done</button><button name="delete">Delete</button><input type="hidden" value="' + rule.id + '"/></td>');
 	$('button[name=done]').bind('click', save_rule);
 	$('button[name=delete]').bind('click', remove_rule);
 }
@@ -107,16 +109,16 @@ function save_rule(event) {
 	var u2sMappings = jsonObj.u2sMappings;
 	var tr = $(event.target).parent().parent();
 	var url = tr.children().slice(0, 1).children().slice(0, 1).attr('value');
-	var search_id = tr.children().slice(1, 2).children().slice(0, 1).attr('value');
+	var type = tr.children().slice(1, 2).children().slice(0, 1).attr('value');
 
-	if(url == undefined || url.trim() == '' || search_id == undefined || search_id.trim() == '') {
+	if(url == undefined || url.trim() == '' || type == undefined || type.trim() == '') {
 		return;
 	}
 
 	for(var i = 0; i < u2sMappings.length; i++) {
 		if(u2sMappings[i].id == id) {
 			u2sMappings[i].url = url;
-			u2sMappings[i].search_id = search_id;
+			u2sMappings[i].type= type;
 			break;
 		}
 	}
--- s2s.js
+++ s2s.js
@@ -21,22 +21,39 @@ function keyPress() {
 		chrome.extension.sendRequest({'type': 'get_map_data'}, function(response){
 				var u2sMappings = response.u2sMappings;
 				for(var i = 0; i < u2sMappings.length; i++) {
-				var reg = new RegExp(u2sMappings[i].url)
-				if (document.URL.match(reg)) {
-				searchQuery = document.getElementById(u2sMappings[i].search_id);
-				if(searchQuery != null && document.activeElement != searchQuery && document.activeElement.nodeName != 'TEXTAREA' && document.activeElement.nodeName != 'INPUT') {
-				setTimeout('focusQuery()', 10);
+					var match = false;	
+					if(u2sMappings[i].type == 'regex' ) {
+						match = document.URL.match(new RegExp(u2sMappings[i].url));
+					} else {
+						match = (document.URL == u2sMappings[i].url);
+					}
+					if (match) {
+						var indexes = u2sMappings[i].tree_route.split(' ');
+						searchQuery = $('body');
+						for(var j = 0; j < indexes.length - 1; j++) {
+							searchQuery = searchQuery.children().eq(indexes[j]);
+						}
+						searchQuery = searchQuery.get(0);
+						if(searchQuery != null && document.activeElement != searchQuery && document.activeElement.nodeName != 'TEXTAREA' && document.activeElement.nodeName != 'INPUT') {
+							setTimeout('focusQuery()', 10);
+						}
+					}
 				}
-				}
-				}
-				});
+			}
+		);
 	}
 }
 
 function send_ele_id(event) {
 	if(event.button == 2) {
-		var id = event.target.id;
-		chrome.extension.sendRequest({'type': 'send_ele_id', 'id': id})			
+		var tree_route = '';
+		var ele = $(event.target);
+
+		while (ele.attr('nodeName') != 'BODY') {
+			tree_route = ele.prevAll().size() + ' ' + tree_route;
+			ele = ele.parent();
+		}
+		chrome.extension.sendRequest({'type': 'send_ele_id', 'tree_route': tree_route})			
 	}
 }
 
